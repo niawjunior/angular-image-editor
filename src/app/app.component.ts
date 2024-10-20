e1d85d3d8ad127bd3ca1c57d96ea9c2fba450aec
niaw-iiv <psb@iiv.co.th>
2024-10-20 14:19:00 +0700

    fix: image url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   title = 'angular-fabric-js';
   mobileDisplay = false;
   damage = null;
-  imageURL = 'http://192.168.1.120:4200/car.jpg';
+  imageURL = 'https://angular-image-editor.netlify.app/car.jpg';
   constructor(private deviceService: DeviceService) {
     const damage = localStorage.getItem('damage');
     this.damage = damage ? JSON.parse(damage) : null;
